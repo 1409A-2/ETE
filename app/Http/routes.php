ec83ef7a339bcab415f2a2212ca0bcd1867abe4d
Your Name <you@example.com>
2016-09-03 13:55:19 +0800

    2016-9-3-company 基本的公司信息 以及 详细公司信息

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -36,4 +36,26 @@ Route::group(['middleware' => ['web']], function () {
 	Route::get('register.html','Index\loginController@register');
 	Route::post('registerPro','Index\loginController@registerPro');
 	Route::post('loginPro','Index\loginController@loginPro');
+
+	//用户个人信息
+	Route::group(['middleware' => 'login'], function () {
+
+		//完善公司基本信息
+		Route::get('info','Index\InfoController@checkCompany');
+
+		Route::get('sendEamil','Index\InfoController@sendMail');
+
+		Route::post('company1pro','Index\InfoController@company1Pro');
+
+		Route::post('company2pro','Index\InfoController@company2Pro');
+
+		//完善公司的信息
+		Route::get('detailed','Index\DetailedController@index');
+
+		Route::post('info1pro','Index\DetailedController@Info1Pro');
+
+	});
+
+	//通过邮箱验证
+	Route::get('adopt','Index\InfoController@adoptVerify');
 });
