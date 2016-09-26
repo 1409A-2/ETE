f20fe434b1c3f8877aa3d21a1992146ccd02e466
Your Name <you@example.com>
2016-09-26 20:24:47 +0800

    公司邮件

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -94,6 +94,7 @@ Route::group(['middleware' => ['web']], function () {
 			Route::get('haveNoticeResumes','Index\IndustryController@haveNoticeResumes');//查看已发送邮件的简历
 			Route::get('haveRefuseResumes','Index\IndustryController@haveRefuseResumes');//查看不合适的简历
 			Route::get('preview','Index\IndustryController@preview');//公司查看简历详情
+			Route::get('undeterminedOffer','Index\IndustryController@undeterminedOffer');//公司发送Offer给用户   undeterminedOffer
 			Route::get('positions','Index\IndustryController@positions');//查看有效职位  positions
 			Route::get('positionsdown','Index\IndustryController@positionsDown');//查看有效职位  positionsdown
 			Route::get('positionsType','Index\IndustryController@positionsType');//职位上下线管理	positionsType
@@ -117,6 +118,7 @@ Route::group(['middleware' => ['web']], function () {
 			Route::get('companyAllBeat', 'Index\IndustryController@companyAllBeat');//公司查看一拍页面   
 			Route::get('companyBeat', 'Index\beatController@companyBeat');//查看一拍页面信息
 			Route::get('beatYes', 'Index\beatController@beatYes');//添加公司的一拍   
+			Route::get('companyBeatEmail', 'Index\IndustryController@companyBeatEmail');// 公司发送offer
 		});
         Route::post('getcollected','Index\IndexController@getCollected');// 是否收藏职位
         Route::post('collectionPosition','Index\IndexController@collectionPosition');// 收藏职位
